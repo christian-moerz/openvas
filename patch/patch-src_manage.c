b489b1959ca01af96561e4d6f04a5842aba1921f
Christian Moerz <freebsd@ny-central.org>
2022-10-16 11:41:29 +0000

    adding patch file for gvmd

---
+++ patch/patch-src_manage.c
@@ -0,0 +1,19 @@
+--- src/manage.c.orig	2022-07-21 07:20:24 UTC
++++ src/manage.c
+@@ -76,6 +76,7 @@
+ #include <sys/file.h>
+ #include <sys/stat.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <sys/wait.h>
+ #include <time.h>
+ #include <unistd.h>
+@@ -5952,6 +5953,8 @@ manage_read_info (gchar *type, gchar *uid, gchar *name
+ int
+ validate_username (const gchar * name)
+ {
++  if (!strcmp("admin", name))
++    return 0;
+   if (g_regex_match_simple ("^[[:alnum:]-_.]+$", name, 0, 0))
+     return 0;
+   else
